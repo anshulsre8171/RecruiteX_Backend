5c8bb55955a8dfa435b1ab701192084310434a59
anshulsre8171 <anshulsre8171@gmail.com>
2025-06-01 22:54:00 +0530

    backend

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv=require("dotenv")
+dotenv.config()
+const expressFileupload = require('express-fileupload');
+const { recruiterRoute } = require('./routes/recruiterRoute')
+const { seekerRoute } = require('./routes/seekerRoute')
+const { AdminRoute } = require('./routes/AdminRoute')
+const app = express();
+app.use(express.json());
+app.use(cors())
+app.use("/upload", express.static("./uploads"));
+//http://localhost:9000/upload/daya.jpg
+app.use(expressFileupload())
+
+
+const DbConnect = async () => {
+    // const con = await mongoose.connect("mongodb://localhost:27017/recruitex");
+     const con = await mongoose.connect(process.env.MONGODB_URI)
+    if (con) {
+        console.log("Connected to MongoDB...");
+    }
+}
+DbConnect();
+app.use("/api", AdminRoute);
+app.use("/api", recruiterRoute);
+app.use("/api", seekerRoute);
+
+
+app.listen(process.env.MONGODB_PORT || 9000, () => {
+    console.log("Server is Running at 9000 port")
+})
